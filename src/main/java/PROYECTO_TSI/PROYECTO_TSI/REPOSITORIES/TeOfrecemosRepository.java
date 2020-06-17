13e28a3f3ef8245fbae9fa442a5aa12978eca3d3
Raquel Montero <nabogaria@gmail.com>
2020-06-17 03:27:13 -0400

    to repo

--- src/main/java/PROYECTO_TSI/PROYECTO_TSI/REPOSITORIES/TeOfrecemosRepository.java
+++ src/main/java/PROYECTO_TSI/PROYECTO_TSI/REPOSITORIES/TeOfrecemosRepository.java
@@ -3,9 +3,10 @@ package PROYECTO_TSI.PROYECTO_TSI.REPOSITORIES;
 import PROYECTO_TSI.PROYECTO_TSI.MODELS.Mision;
 import PROYECTO_TSI.PROYECTO_TSI.MODELS.TeOfrecemos;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface TeOfrecemosRepository extends CrudRepository<TeOfrecemos,Integer> {
     List<TeOfrecemos> findAll();
     TeOfrecemos findById(int id);
