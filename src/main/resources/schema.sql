4043666a8162a4c125c8dcf517b70bafcbb069a1
Raquel Montero <nabogaria@gmail.com>
2020-06-26 17:53:16 -0400

    firs commit to redheroku

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,3 @@
+create  TABLE if not exists Mision (id_mision integer not null auto_increment, contenido varchar(2000), primary key (id_mision))
+
+INSERT INTO mision(id_mision,contenido) VALUES (2,'mision')
